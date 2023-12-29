2e96c0d341d805249c9e212e13698148d1159900
Shayan Shabanzadeh <shayan.shabanzadeh@peykasa.ir>
2023-12-29 18:11:17 +0330

    add: sumOfSquares function

--- src/main/java/JavaCup.java
+++ src/main/java/JavaCup.java
@@ -2,8 +2,7 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class JavaCup {
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Press number1: ");
         int i = scanner.nextInt();
@@ -11,17 +10,19 @@ public class JavaCup {
         int j = scanner.nextInt();
         System.out.println("Press number3: ");
         int k = scanner.nextInt();
+        sumOfSquares(100000);
         temp();
         eval(i, j, k);
     }
-    public static void eval(int i, int j, int k)
-    {
-        if (i * i + j * j == k * k || i * i == j * j + k * k || j * j == i * i + k * k)
-        {
+
+    public static void eval(int i, int j, int k) {
+        if (i * i + j * j == k * k || i * i == j * j + k * k || j * j == i * i + k * k) {
             System.out.println("YES");
+        } else {
+            System.out.println("NO");
         }
-        else { System.out.println("NO"); }
     }
+
     public static void temp() {
         ArrayList<Integer> a = new ArrayList<>(10000 * 2000);
 
@@ -31,4 +32,15 @@ public class JavaCup {
             }
         }
     }
+
+    public static void sumOfSquares(int limit) {
+        int sum;
+        ArrayList<Integer> a = new ArrayList<>(limit);
+        for (int i = 1; i <= limit; i++) {
+            a.add(i * i);
+        }
+        sum = a.stream().mapToInt(e -> e).sum();
+        System.out.println("sum is : " + sum);
+    }
+
 }
